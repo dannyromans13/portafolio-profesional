cfeddf9c285555c2ed12542de4644a2732e77e72
dannyromans13 <dannyromans13@gmail.com>
2026-08-11 18:23:39 -0600

    fix: show header with back button on Project Detail screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,12 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="project/[id]"
+        options={{ title: "Detalle del Proyecto" }}
+      />
+    </Stack>
   );
 }
